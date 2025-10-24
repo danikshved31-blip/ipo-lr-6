c7218b218932923e732eee6f5c8f329f783c4aff
danchik <danikshved31@gmail.com>
2025-10-24 10:13:46 +0300

    bimbimbambam

--- task_4/main.py
+++ task_4/main.py
@@ -1,1 +1,12 @@
-
+text = input("введите текст который будем искать: ")
+spisok = []
+sum_strok = 0
+with open('text.txt', 'r', encoding=' utf-8 ') as a:
+    lines = a.readlines()
+    for line in lines:
+        if text in line:
+            spisok.append(line.strip())
+            sum_strok += 1
+spisok.sort(key=len)
+print(spisok)
+print(f"строчек с этим текстом {sum_strok}")
